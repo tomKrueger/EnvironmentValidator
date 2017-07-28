c7f12ce61402441ce4341423ed9d7edb1818e1ee
Tom Krueger <1824935+tomkrueger@users.noreply.github.com>
2017-07-28 07:40:22 -0500

    Added EnvironmentValidator Console App (.NET Core) a

---
+++ Source/EnvironmentValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvironmentValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
